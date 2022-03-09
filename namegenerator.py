c65dc397be7343b6e4be2b1e0240120708c6bb1c
Michael Scott <84039563+thebluenowhere@users.noreply.github.com>
2022-03-09 09:25:15 +0000

    Update namegenerator.py

--- namegenerator.py
+++ namegenerator.py
@@ -1,9 +1,9 @@
 import random 
-
+name = input(f"What's the name?!")
 nation = random.choice(open('nationalities.txt').readlines()).replace('\n', '')
 adjective = random.choice(open('adjectives.txt').readlines()).replace('\n','')
 animal = random.choice(open('animals.txt').readlines()).replace('\n', '')
 profession = random.choice(open('professions.txt').readlines()).replace('\n','')
 profession = profession[:-1]
 
-print(f"Ah, yes. Andy. Also known as The {adjective} {animal} {profession}! He's {nation}!"
+print(f"Ah, yes. {name}.title(). Also known as The {adjective} {animal} {profession}! He's {nation}!"
